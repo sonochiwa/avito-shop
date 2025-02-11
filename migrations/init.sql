789338ca8d533793a05033334721b308ba557d89
Рашид Мамаримов <sonochiwaxd@gmail.com>
2025-02-12 04:09:45 +0600

    refactor

--- migrations/init.sql
+++ migrations/init.sql
@@ -1,12 +1,13 @@
 CREATE TABLE users
 (
     id         SERIAL PRIMARY KEY,
-    username   VARCHAR(64) NOT NULL,
-    password   VARCHAR(64) NOT NULL,
-    balance    INT         NOT NULL DEFAULT 1000,
-    created_at TIMESTAMP   NOT NULL DEFAULT now(),
+    username   VARCHAR(32)  NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    balance    INT          NOT NULL CHECK (balance >= 0),
+    created_at TIMESTAMP    NOT NULL DEFAULT now(),
     CONSTRAINT username_unique UNIQUE (username)
 );
+CREATE INDEX idx_users_username ON users (username);
 
 CREATE TABLE products
 (
@@ -15,6 +16,7 @@ CREATE TABLE products
     price INT         NOT NULL,
     CONSTRAINT title_unique UNIQUE (title)
 );
+CREATE INDEX idx_products_title ON products (title);
 
 CREATE TABLE inventory
 (
@@ -23,6 +25,8 @@ CREATE TABLE inventory
     quantity   INTEGER NOT NULL,
     CONSTRAINT user_id_product_id_unique UNIQUE (user_id, product_id)
 );
+CREATE INDEX idx_inventory_user_id ON inventory (user_id);
+CREATE INDEX idx_inventory_product_id ON inventory (product_id);
 
 CREATE TABLE ledger
 (
@@ -32,6 +36,8 @@ CREATE TABLE ledger
     amount     INT       NOT NULL CHECK (amount > 0),
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
+CREATE INDEX idx_ledger_from_user ON ledger (from_user);
+CREATE INDEX idx_ledger_to_user ON ledger (to_user);
 
 INSERT INTO products (title, price)
 VALUES ('t-shirt', 80),
